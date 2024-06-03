07b323067d572050d02bbabd180ae35da946cb34
NatiEfraim <nati.efraim@gmail.com>
2024-06-03 12:17:17 +0300

    Added swagger on ClientController on index function

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -11,7 +11,52 @@ use Illuminate\Support\Facades\Log;
 class ClientController extends Controller
 {
     //
-
+    /**
+     * @OA\Get(
+     *      path="/api/clients",
+     *      tags={"Clinet"},
+     *      summary="Get all clinets records name and id",
+     *      description="Retrieves all clinets from the system only name and id.",
+     *      @OA\Parameter(
+     *          name="page",
+     *          in="query",
+     *          description="Page number for pagination",
+     *          required=false,
+     *          @OA\Schema(
+     *              type="integer",
+     *              default=1
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success response",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(
+     *                  type="object",
+     *                  @OA\Property(property="id", type="integer", example=1),
+     *                  @OA\Property(property="name", type="string", example="Percival Schulist"),
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="message", type="string", example="משתמש אינו מחובר")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="message", type="string", example="התרחש בעיית שרתת יש לנסות שוב מאוחר יותר.")
+     *          )
+     *      )
+     * )
+     */
     public function index()
     {
 
