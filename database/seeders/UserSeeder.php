e24680dc9b14e7e257a8235059f43c1739aa3c98
NatiEfraim <nati.efraim@gmail.com>
2024-06-30 11:12:29 +0300

    Insert Roie as adin in the system

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,13 +4,14 @@ namespace Database\Seeders;
 
 use App\Models\EmployeeType;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 use Spatie\Permission\Models\Role;
 use Faker\Factory as Faker;
-// use Illuminate\Support\Str;
 // use Illuminate\Support\Facades\Hash;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Support\Str;
 
 
 
@@ -21,7 +22,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         //
         // Create an instance of the Faker library
         $faker = Faker::create('he_IL');
@@ -29,32 +30,45 @@ class UserSeeder extends Seeder
         // Fetch all roles with the correct guard
         $roles = Role::where('guard_name', 'passport')->pluck('name')->toArray();
 
-        for ($i = 0; $i < 10; $i++) {
+        $user =User::create([
+
+            'name' => 'רועי סחייק',
+            'personal_number' => '9804761',
+            'email' => 'c9804761@army.idf.il',
+            'emp_type_id' => 4,
+            'phone' => '0545452493',
+            // 'remember_token' => Str::random(10),
+
+        ]);
+
+        $role= Role::where('name', 'admin')->first();
+        $user->assignRole($role);//give admin role
+
+        // for ($i = 0; $i < 10; $i++) {
             
-            // Generate random user data
-            $randomEmpType = EmployeeType::inRandomOrder()->first();
-            $personalNumber = $faker->unique()->numberBetween(1000000, 9999999);
-            $letters = ['s', 'c', 'm'];
-            $randomLetter = $faker->randomElement($letters);
-            $email = "{$randomLetter}{$personalNumber}@army.idf.il";
-            $phone = $faker->unique()->regexify('05\d{8}');
+        //     // Generate random user data
+        //     $randomEmpType = EmployeeType::inRandomOrder()->first();
+        //     $personalNumber = $faker->unique()->numberBetween(1000000, 9999999);
+        //     $letters = ['s', 'c', 'm'];
+        //     $randomLetter = $faker->randomElement($letters);
+        //     $email = "{$randomLetter}{$personalNumber}@army.idf.il";
+        //     $phone = $faker->unique()->regexify('05\d{8}');
 
-            // Create user
-            $user = User::create([
-                'name' => $faker->name(),
-                'email' => $email,
-                'personal_number' => $personalNumber,
-                'emp_type_id' => $randomEmpType->id,
-                'phone' => $phone,
-                //   'password' => Hash::make('password'), // Add password
-                //   'remember_token' => Str::random(10),
-            ]);
+        //     // Create user
+        //     $user = User::create([
+        //         'name' => $faker->name(),
+        //         'email' => $email,
+        //         'personal_number' => $personalNumber,
+        //         'emp_type_id' => $randomEmpType->id,
+        //         'phone' => $phone,
+        //         //   'password' => Hash::make('password'), // Add password
+        //         //   'remember_token' => Str::random(10),
+        //     ]);
 
-            // Assign random role to the user
-            $randomRole = $faker->randomElement($roles);
-            $user->assignRole($randomRole);
-        }
-
+        //     // Assign random role to the user
+        //     $randomRole = $faker->randomElement($roles);
+        //     $user->assignRole($randomRole);
+        // }
 
 
 
