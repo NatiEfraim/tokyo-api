186fa48a12b91a7621684c40bf6c572f45f96513
NatiEfraim <nati.efraim@gmail.com>
2024-09-09 09:41:27 +0300

    Added getHebrewMessage function on Status Enum

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -15,4 +15,30 @@ enum Status:int
     case CONFLICT = 409;
     case FORBIDDEN = 403;
     case NO_CONTENT = 204;
+
+
+        /**
+     * Get the Hebrew message for each status error.
+     *
+     * @return string
+     */
+    public function getHebrewMessage(): string
+    {
+        return match ($this) {
+            self::OK => 'בקשה הושלמה בהצלחה.',
+            self::NOT_FOUND => 'בקשה לא נמצאה.',
+            self::CREATED => 'הנתונים נשמרו במערכת.',
+            self::BAD_REQUEST => 'בקשה שגויה.',
+            self::UNAUTHORIZED => 'הגישה נדחתה. יש להתחבר למערכת',
+            self::INTERNAL_SERVER_ERROR => 'שגיאה פנימית בשרת.',
+            self::CONFLICT => 'קיימת סתירה בנתונים.',
+            self::FORBIDDEN => 'משתמש אינו מורשה לבצע בקשה זו.',
+            self::NO_CONTENT => 'תוכן לא נמצא במערכת.',
+            self::UNPROCESSABLE_ENTITY => 'נתונים שנשלחו אינם תקינים.',
+        };
+    }
+
+
+
+
 }
